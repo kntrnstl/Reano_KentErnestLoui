e6e91fb8d696fcf7ebb0ca350cd13a805c844d49
Kent Ernest Loui Reano <louireano@gmail.com>
2025-09-09 20:29:58 +0800

    First commit - add CRUD project

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8623ab5c3a8321d1df41ac35ce4b23e5d5e0b0af',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8623ab5c3a8321d1df41ac35ce4b23e5d5e0b0af',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
